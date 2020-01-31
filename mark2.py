23a6085b29cc1fe09d63c567bbf8f63524b52d50
RevantR <revanth.rallabandi@gmail.com>
2020-01-31 14:23:13 +0530

    Changes made to the Repo

--- mark2.py
+++ mark2.py
@@ -1,15 +1,30 @@
 import pandas as pd
 import csv
 import glob
+import os
 
-with open('EAM_report_summary.csv','w',newline='') as file:
+dir_name = input("Enter the folder name : ")
+
+#Rename all the files in the folder with resect to the folder name
+
+i = 1
+dir_name_list=dir_name.split()
+new_name='_'.join(dir_name_list)
+for filename in os.listdir(dir_name):
+    dst= dir_name + "/" + new_name +"_Report_"+str(i)+".csv"
+    src= dir_name+'/'+filename
+    os.rename(src,dst)
+    i+=1 
+
+# Create a new csv file to store the summary
+
+with open(new_name + '_Report_Summary.csv','w',newline='') as file:
     writer=csv.writer(file)
     writer.writerow(["Dashboard Name","Analysis Name","User Count","Query Count","User Count(Jan)","Query Count(Jan)","Job Function"])
 
-files= glob.glob('test/*.csv')
-
 # Read Multiple CSV Files in a given folder
 
+files = glob.glob(dir_name+'/*.csv')
 for file in files:
     df=pd.read_csv(file)
     date=df['Date']
@@ -41,22 +56,24 @@ for file in files:
     user_count_jan=len(names_user_jan)
     query_count_jan=sum(query_count[:len(new_date_jan)])
 
-    # Get the dashboard Name and the Report Name
+    # Get the Dashboard Name and the Report Name
 
     report_name=df['Report/Analysis']
     report_name=list(report_name)
     report_name[0]=report_name[0].split('/')
-
-    dashboard_name=report_name[0][-2]
     analysis_name=report_name[0][-1]
 
+    dashboard=df['Dashboard']
+    dashboard=list(dashboard)
+    dashboard[0]=dashboard[0].split('/')
+    dashboard_name= dashboard[0][-1]
+   
     # Get the Job Function
-
+ 
     job_func=df['JOB Function']
     job_func=set(list(job_func))
     job_func=list(job_func)
     job_func=', '.join(job_func)
-
     print(dashboard_name,analysis_name, names_user_count,query_count_val, user_count_jan, query_count_jan, job_func)
 
     # Write the list into a csv file
@@ -70,7 +87,7 @@ for file in files:
     new_row.append(query_count_jan)
     new_row.append(job_func)   
 
-    with open('EAM_report_summary.csv','a',newline='') as file:
+    with open(new_name+'_Report_Summary.csv','a',newline='') as file:
         writer=csv.writer(file)
         writer.writerow(new_row)
         
