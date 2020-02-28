6cbaec47006e36087405a1ae87343cb05314e8f2
RevantR <revanth.rallabandi@gmail.com>
2020-02-29 01:03:37 +0530

    Each Month Almost Over

--- Each_Month.py
+++ Each_Month.py
@@ -2,49 +2,26 @@ import pandas as pd
 import csv
 import glob
 import os
-
-month_list = ['January','February','March','April','May','June','July','August','September','October','November','December']
-month_list_resp=month_list #copy of the list to evaluate input
-
-month_list = list(enumerate(month_list,1))
+from itertools import chain
 
 dir_name = input("Enter the folder name : ")
-month_name = input("Enter the month to get the statistics : ") 
 while dir_name.lower() not in list(map(lambda x:x.lower(),os.listdir())):
     dir_name = input("Please Re-enter Folder Name : ")
-temp=[]
-
-while len(temp) == 0:
-    for i in month_list_resp:
-        if month_name.lower() in i.lower():
-            temp.append(i)
-    if len(temp)==1:
-        print(temp)
-        break
-    else:
-        month_name = input("Please Re-Enter the month to get the statistics : ") 
-
-
-month_num = 0
-for i in range(len(month_list)):
-    if month_name.lower() in month_list[i][1].lower():
-        month_num = month_list[i][0] 
 
 #Rename all the files in the folder with resect to the folder name
-i = 1
-dir_name_list = dir_name.split()
-new_name='_'.join(dir_name_list)
-for filename in os.listdir(dir_name):
-    dst = dir_name + "/" + new_name +"_Analysis_July_"+str(i)+".csv"
-    src = dir_name+'/'+filename
-    os.rename(src,dst)
-    i+=1 
+# i = 1
+# dir_name_list = dir_name.split()
+# new_name='_'.join(dir_name_list)
+# for filename in os.listdir(dir_name):
+#     dst = dir_name + "/" + new_name +"_Analysis_July_"+str(i)+".csv"
+#     src = dir_name+'/'+filename
+#     os.rename(src,dst)
+#     i+=1 
 
 # Create a new csv file to store the summary
-month_name=temp[0]
-with open(new_name + '_Report_Summary.csv','w',newline='') as file:
+with open( 'Report_Summary.csv','w',newline='') as file:
     writer=csv.writer(file)
-    writer.writerow(["Dashboard Name","Analysis Name","User Count","Query Count","User Count("+ month_name +")","Query Count("+ month_name+" 2019)","Job Function"])
+    writer.writerow(["Dashboard Name","Analysis Name","User Count Year","Query Count Year","Jan_User","Feb_User","Mar_User","Apr_User","May_User","Jun_User","July_User","Aug_User","Sep_User","Oct_User","Nov_User","Dec_User","Jan_Query","Feb_Query","Mar_Query","Apr_Query","May_Query","Jun_Query","July_Query","Aug_Query","Sep_Query","Oct_Query","Nov_Query","Dec_Query","Job_function","User Names"])
 
 # Read Multiple CSV Files in a given folder
 
@@ -62,19 +39,28 @@ for file in files:
     names_user = list(df['Full Name'])
     names_user_count=len(set(names_user))
     names_user_mon=[]
-
+    dict_month_query_count={1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0}
+    dict_month_user={1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[],10:[],11:[],12:[]}
+    user_count_month=[]
+    user_count_year=[]
     new_date_mon=[]
     for i in range(len(names_user)):
-        if(int(date[i][5:7])) == int(month_num):
-            names_user_mon.append(names_user[i])
-            query_count_mon.append(query_count[i])
+        for j in range(1,13):
+            if(int(date[i][5:7])) == j:
+                dict_month_query_count[j]+=query_count[i]
+                dict_month_user[j].append(names_user[i])
+    for i in range(1,13):
+        dict_month_user[i]=list(set(dict_month_user[i]))
+    for i in dict_month_user.values():
+        user_count_month.append(len(i))
+        user_count_year.append(i)
 
-    names_user_mon=set(names_user_mon)
-    user_count_mon=len(names_user_mon)
-    query_count_mon_val=sum(query_count_mon)
-
-    # Get the Dashboard Name and the Report Name
-
+    # calculate user and query count
+    user_count_year=list(chain(*user_count_year))
+    user_year=list(set(user_count_year))
+    user_count_year=len(set(user_count_year))
+    month_query_count=list(dict_month_query_count.values())
+    year_query_count=sum(month_query_count)
     report_name=df['Report/Analysis']
     report_name=list(report_name)
     report_name[0]=report_name[0].split('/')
@@ -91,20 +77,24 @@ for file in files:
     job_func=set(list(job_func))
     job_func=list(job_func)
     job_func=', '.join(job_func)
-    print(dashboard_name,analysis_name, names_user_count,query_count_val, user_count_mon, query_count_mon_val, job_func)
+
+    #Get the user
+    user_year='/ '.join(user_year)
+    # print(dashboard_name,analysis_name, names_user_count,query_count_val, user_count_mon, query_count_mon_val, job_func)
 
     # Write the list into a csv file
 
     new_row=[]
     new_row.append(dashboard_name)
     new_row.append(analysis_name)
-    new_row.append(names_user_count)
-    new_row.append(query_count_val)
-    new_row.append(user_count_mon)
-    new_row.append(query_count_mon_val)
+    new_row.append(user_count_year)
+    new_row.append(year_query_count)
+    new_row=new_row+user_count_month
+    new_row=new_row+(month_query_count)
     new_row.append(job_func)   
-
-    with open(new_name+'_Report_Summary.csv','a',newline='') as file:
+    new_row.append(user_year)
+    print(new_row)
+    with open('Report_Summary.csv','a',newline='') as file:
         writer=csv.writer(file)
         writer.writerow(new_row)
         
